621576faa166e8f9abed14080cbe77ee5059ed4e
hyebin <bin930827@gmail.com>
2023-07-28 17:36:54 +0900

    swiper

--- script/script.js
+++ script/script.js
@@ -27,3 +27,31 @@ close.addEventListener('click',function(){
     //모바일메뉴 숨기기
     m_nav_open.style.display = 'none';
 })
+// main-swiper-slide
+// const 변수명 = new Swiper('적용대상');
+// const 변수명 = new Swiper('적용대상',{속성:값, 속성:값});
+const pawin_slide =new Swiper('#pawin_slide',{
+    autoplay:{
+        delay:2000,//슬라이드 간격(밀리초) 기본 3초
+        disableOnInteraction:false,//버튼 클릭 후 자동재생유지
+    },//자동재생
+    loop:true, //마지막 슬라이드 ->첫번째 슬라이드 자연스러운 변경
+    effect:'fade',//제자리 변경 슬라이드 효과
+    // direction:'vertical'
+    navigation: {//이전,다음 내비게이션 연결
+        //next,prev 객체 연결시 부모를 안 적으면
+        //body 안에 있는 모든 swiper-next,prev를 인식하기때문에
+        //개별인식 가능한 부모 이름을 반드시 앞에 먼저 작성한다.
+        nextEl: '#pawin_slide .swiper-button-next',
+        prevEl: '#pawin_slide .swiper-button-prev',
+    },
+});
+const pawinSlide =new Swiper('#pawin_slide2',{
+    autoplay:{delay:4000},
+    loop:true,
+    direction:'vertical',
+    navigation: {
+        nextEl: '#pawin_slide2 .swiper-button-next',
+        prevEl: '#pawin_slide2 .swiper-button-prev',
+    },
+})
